315feec4cde9c943d6b1d70742a481e5ce5bbcff
Christian <christabanao331@gmail.com>
2026-06-29 22:56:58 +0800

    SB-3 Prisma Update & Seed Setup: Updated Prisma Versions and Added seed.ts sample data.

--- web/src/lib/prisma.ts
+++ web/src/lib/prisma.ts
@@ -1,7 +1,13 @@
-import { PrismaClient } from "../../prisma/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-export const prisma = new PrismaClient({ adapter });
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
